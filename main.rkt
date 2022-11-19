e782a9ec90d9713212a5fbf6d4131cd167a414a8
Ardacuaa <61476686+Ardacuaa@users.noreply.github.com>
2022-11-18 21:18:33 -0500

    Add files via upload

---
+++ main.rkt
@@ -0,0 +1,15 @@
+#lang racket
+(require "utility.rkt")
+(require "runner1.rkt")
+(require "parcer.rkt")
+
+
+
+(define env '((global (a 1) (b 2) (c 5))))
+
+
+
+(define sample-code '(block (print a) (assign x 8) (assign y (math * x 2)) (print y) (assign z (math + b y)) (print z)))  ;no output
+;(displayln (neo-parser sample-code))
+(define parsed-neo-code (neo-parser sample-code))
+(run-neo-parsed-code parsed-neo-code env)
